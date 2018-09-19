95583ed692e80487b062c12111d8edf562e273f9
teclxl <11850956+teclxl@users.noreply.github.com>
2018-09-19 22:22:20 +0800

    切换许可证
         因为使用了采用Apache 2.0许可证的源码，所以不能再继续采用MIT许可协议

--- chaos-supports/chaos-support-domain/src/main/java/xyz/devlxl/chaos/support/domain/AbstractAggregateRoot.java
+++ chaos-supports/chaos-support-domain/src/main/java/xyz/devlxl/chaos/support/domain/AbstractAggregateRoot.java
@@ -1,6 +1,7 @@
 /*
  * @formatter:off
- * Copyright 2018 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
+ * Copyright 2018 teclxl
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
